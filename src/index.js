25bcde8459973a32a997a328597483ddf8530788
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-07-24 19:57:20 -0300

    fix src/index

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
+"use strict";
+
 const symbolListWithFourLetters = require('./jsons/symbolListWithFourLetters');
 const symbolListWithFiveLetters = require('./jsons/symbolListWithFiveLetters');
 
-module.exports = function format(value) {
-    if(typeof value !== 'string') {
+function format(value) {
+    if (typeof value !== 'string') {
         throw new Error("Report type error valid string");
-    } 
+    }
 
     switch (value.length) {
         case 6:
             return `${value.substring(0, 3)}/${value.substring(3)}`;
         case 7:
-            const symbol = symbolListWithFourLetters.filter(symbol => value.includes(symbol)[0]);
+            const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
             const splittedValue = value.split(symbol);
             return `${splittedValue[0]}/${splittedValue[1]}`;
         case 8:
-            const symbol = symbolListWithFourLetters.filter(symbol => value.includes(symbol)[0]);
+            const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
             const splittedValue = value.split(symbol);
             return `${splittedValue[0]}/${splittedValue[1]}`;
-    
+
         default:
-            return throw new Error("Invalid symbol, enter a valid symbol to perform the formatting")
-            break;
-    }
-
-
-    if(value.length === 6) {
-        return `${value.substring(0, 3)}/${value.substring(3)}`;
-    }
-    if(value.length === 7) {
-        const symbol = symbolListWithFourLetters.filter(symbol => value.includes(symbol)[0]);
-        const splittedValue = value.split(symbol);
-        return `${splittedValue[0]}/${splittedValue[1]}`;
-    }
-    if(value.length === 8) {
-        const symbol = symbolListWithFourLetters.filter(symbol => value.includes(symbol)[0]);
-        const splittedValue = value.split(symbol);
-        return `${splittedValue[0]}/${splittedValue[1]}`;
+        //return throw new Error("Invalid symbol, enter a valid symbol to perform the formatting");
     }
 }
+
+const test1 = format("ETHBTC");
+const test2 = format("BTCUSTD");
+const test3 = format("ETHCLOAK");
+
+console.log(test1, test2, test3);
+
+module.exports = format;
+
+

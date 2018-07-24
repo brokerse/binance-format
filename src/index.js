a9bee70e2e4b570cff3d46cabac6bea35ff65b02
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-07-24 20:22:29 -0300

    improve src/index and add jest

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ const symbolListWithFourLetters = require('./jsons/symbolListWithFourLetters');
 const symbolListWithFiveLetters = require('./jsons/symbolListWithFiveLetters');
 
 function formatWithFourLetters(value) {
-    const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
-    const splittedValue = value.split(symbol);
-    return `${splittedValue[0]}/${splittedValue[1]}`;
+    const initialValue = value.substring(0, 4);
+
+    return symbolListWithFourLetters.includes(initialValue)
+        ? `${initialValue}/${value.substring(4)}`
+        : `${value.substring(0, 3)}/${value.substring(3)}`;
 }
 
 function formatWithFiveLetters(value) {
-    const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
-    const splittedValue = value.split(symbol);
-    return `${splittedValue[0]}/${splittedValue[1]}`;
+    const initialValue = value.substring(0, 5);
+
+    return symbolListWithFourLetters.includes(initialValue)
+        ? `${initialValue}/${value.substring(5)}`
+        : `${value.substring(0, 3)}/${value.substring(3)}`;
 }
+
 
 function format(value) {
     if (typeof value !== 'string') {
@@ -33,7 +38,7 @@ function format(value) {
 }
 
 const test1 = format("ETHBTC");
-const test2 = format("BTCUSTD");
+const test2 = format("BTCUSDT");
 const test3 = format("ETHCLOAK");
 
 console.log(test1, test2, test3);

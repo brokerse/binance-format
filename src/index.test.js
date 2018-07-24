33d009452e2ebcf9f2598b7c848015d205449770
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-07-24 20:37:37 -0300

    src/index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -11,3 +11,15 @@ test("should format the BTCUSDT value for BTC/USDT", () => {
 test("should format the ETHCLOAK value for ETH/CLOAK", () => {
     expect(format("ETHCLOAK")).toBe("ETH/CLOAK");
 });
+
+test("should format the BTCETH value for BTC/ETH", () => {
+    expect(format("BTCETH")).toBe("BTC/ETH");
+});
+
+test("should format the USDTBTC value for USDT/BTC", () => {
+    expect(format("USDTBTC")).toBe("USDT/BTC");
+});
+
+test("should format the CLOAKETH value for CLOAK/ETH", () => {
+    expect(format("CLOAKETH")).toBe("CLOAK/ETH");
+});

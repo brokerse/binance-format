e9d5857c0ace9bdba259c04458dfc0729e872a43
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-07-24 20:01:10 -0300

    create formatWithFourLetters and formatWithFiveLetters

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@
 const symbolListWithFourLetters = require('./jsons/symbolListWithFourLetters');
 const symbolListWithFiveLetters = require('./jsons/symbolListWithFiveLetters');
 
+function formatWithFourLetters(value) {
+    const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
+    const splittedValue = value.split(symbol);
+    return `${splittedValue[0]}/${splittedValue[1]}`;
+}
+
+function formatWithFiveLetters(value) {
+    const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
+    const splittedValue = value.split(symbol);
+    return `${splittedValue[0]}/${splittedValue[1]}`;
+}
+
 function format(value) {
     if (typeof value !== 'string') {
         throw new Error("Report type error valid string");
@@ -12,14 +24,9 @@ function format(value) {
         case 6:
             return `${value.substring(0, 3)}/${value.substring(3)}`;
         case 7:
-            const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
-            const splittedValue = value.split(symbol);
-            return `${splittedValue[0]}/${splittedValue[1]}`;
+            return formatWithFourLetters(value);
         case 8:
-            const symbol = symbolListWithFourLetters.filter(s => value.includes(s)[0]);
-            const splittedValue = value.split(symbol);
-            return `${splittedValue[0]}/${splittedValue[1]}`;
-
+            return formatWithFiveLetters(value);
         default:
         //return throw new Error("Invalid symbol, enter a valid symbol to perform the formatting");
     }

15b3016ff4b46452121960e1298a37986b06e21d
Oler <roodrigoprogrammer@gmail.com>
2019-04-28 12:11:28 -0300

    fix format and update version

--- src/format.js
+++ src/format.js
@@ -1,96 +1,98 @@
 const icoListOne = [
-    'AION',
-    'APPC',
-    'ARDR',
-    'BCPT',
-    'CHAT',
-    'DASH',
-    'DATA',
-    'DENT',
-    'FUEL',
-    'IOST',
-    'IOTA',
-    'IOTX',
-    'LEND',
-    'LINK',
-    'LOOM',
-    'MANA',
-    'NANO',
-    'NEBL',
-    'NPXS',
-    'NULS',
-    'PIVX',
-    'POWR',
-    'SALT',
-    'TRIG',
-    'TUSD',
-    'USDT',
-    'VIBE',
-    'WABI',
-    'YOYO',
-    'QTUM',
-    'DOCK',
-]
+    "AION",
+    "APPC",
+    "ARDR",
+    "BCPT",
+    "CHAT",
+    "DASH",
+    "DATA",
+    "DENT",
+    "FUEL",
+    "IOST",
+    "IOTA",
+    "IOTX",
+    "LEND",
+    "LINK",
+    "LOOM",
+    "MANA",
+    "NANO",
+    "NEBL",
+    "NPXS",
+    "NULS",
+    "PIVX",
+    "POWR",
+    "SALT",
+    "TRIG",
+    "TUSD",
+    "USDT",
+    "VIBE",
+    "WABI",
+    "YOYO",
+    "QTUM",
+    "DOCK",
+    "POLY",
+    "CELR"
+];
 
 const icoListTwo = [
-    'BCHSV',
-    'CLOAK',
-    'NCASH',
-    'SNGLS',
-    'STEEM',
-    'STORJ',
-    'STORM',
-    'STRAT',
-    'THETA',
-    'WAVES',
-    'WINGS',
-]
+    "BCHSV",
+    "CLOAK",
+    "NCASH",
+    "SNGLS",
+    "STEEM",
+    "STORJ",
+    "STORM",
+    "STRAT",
+    "THETA",
+    "WAVES",
+    "WINGS"
+];
 
-const icoListFour = ['QTUM', 'TUSD', 'IOTA', 'MITH', 'DOCK']
+const icoListFour = ["QTUM", "TUSD", "IOTA", "MITH", "DOCK", "POLY", "CELR"];
 
 function formatWithTwoLetters(value) {
-    return `${value.substring(0, 2)}/${value.substring(2)}`
+    return `${value.substring(0, 2)}/${value.substring(2)}`;
 }
 
 function formatWithFourLetters(value) {
-    const baseAsset = value.substring(0, 4)
+    const baseAsset = value.substring(0, 4);
 
     return icoListOne.includes(baseAsset)
         ? `${baseAsset}/${value.substring(4)}`
-        : `${value.substring(0, 3)}/${value.substring(3)}`
+        : `${value.substring(0, 3)}/${value.substring(3)}`;
 }
 
 function formatWithFiveLetters(value) {
-    const baseAsset = value.substring(0, 5)
-    const quoteAsset = value.substring(5, 0)
+    const baseAsset = value.substring(0, 5);
+    const quoteAsset = value.substring(5, 0);
 
     return icoListOne.includes(baseAsset)
         ? `${baseAsset}/${value.substring(5)}`
         : icoListTwo.includes(quoteAsset)
         ? `${value.substring(0, 5)}/${value.substring(5)}`
-        : `${value.substring(0, 3)}/${value.substring(3)}`
+        : `${value.substring(0, 3)}/${value.substring(3)}`;
 }
 
 function format(value) {
-    if (typeof value !== 'string') {
-        throw new Error('Report type error valid string')
+    if (typeof value !== "string") {
+        throw new Error("Report type error valid string");
     }
 
     switch (value.length) {
         case 5:
-            return formatWithTwoLetters(value)
+            return formatWithTwoLetters(value);
         case 6:
-            return `${value.substring(0, 3)}/${value.substring(3)}`
+            return `${value.substring(0, 3)}/${value.substring(3)}`;
         case 7:
-            return formatWithFourLetters(value)
+            return formatWithFourLetters(value);
         case 8:
-            const ignore = icoListFour.filter(ico => value.includes(ico))
+            const ignore = icoListFour.filter(ico => value.includes(ico));
             if (ignore.length > 0)
-                return `${value.substring(0, 4)}/${value.substring(4)}`
-            return formatWithFiveLetters(value)
+                return `${value.substring(0, 4)}/${value.substring(4)}`;
+            return formatWithFiveLetters(value);
         default:
-            return value
+            return value;
     }
 }
 
-module.exports = format
+module.exports = format;
